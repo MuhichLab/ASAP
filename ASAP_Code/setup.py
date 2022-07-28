0b960a880a75f380eef2f1e1a6cfc66334f83f31
sawilso6 <103147744+sawilso6@users.noreply.github.com>
2022-07-28 11:09:07 -0700

    Create setup.py

---
+++ ASAP_Code/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='ASAP_Code',
+    version='2022.00.01',
+    author='Steven Wilson',
+    author_email='sawilso6@asu.edu',
+    url='https://github.com/MuhichLab/ASAP/ASAP_Code',
+    packages=['ASAP_Code'],
+)
