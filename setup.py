d983800105afa6e44cafffa8561fbe1223a753d3
sawilso6 <103147744+sawilso6@users.noreply.github.com>
2022-07-28 13:44:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Steven Wilson',
     author_email='sawilso6@asu.edu',
     url='https://github.com/MuhichLab/ASAP',
-    packages=find_packages(),
+    py_modules=['ASAP'],
    )
 
 
