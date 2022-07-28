3cd0f4a28cd1892b40d6fed02e5db53703dce693
sawilso6 <103147744+sawilso6@users.noreply.github.com>
2022-07-28 12:13:11 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ASAP_Code',
+    name='ASAP',
     version='1.0.0',
     author='Steven Wilson',
     author_email='sawilso6@asu.edu',
     url='https://github.com/MuhichLab/ASAP',
     packages=find_packages(),
-   
-)
+    py_modules=['AutomaticSurfaceAdsorbateStructureProvider'],
+   )
