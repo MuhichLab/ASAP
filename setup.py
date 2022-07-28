a524d02c33181ef173fc9577eeb4b903a4f89cb0
sawilso6 <103147744+sawilso6@users.noreply.github.com>
2022-07-28 11:40:12 -0700

    Update and rename ASAP_Code/setup.py to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setuptools.setup(
     version='1.0.0',
     author='Steven Wilson',
     author_email='sawilso6@asu.edu',
-    url='https://github.com/MuhichLab/ASAP/ASAP_Code'
+    url='https://github.com/MuhichLab/ASAP'
+    py_modules=['ASAP_Code'],
 )
